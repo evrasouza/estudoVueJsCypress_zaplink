56a5277fc29bf11528b7599193606c7f4927646e
Everton Souza <evrasouza@gmail.com>
2020-12-15 20:56:03 -0300

    Anexando Screenshots no Report

--- frontend/tests/e2e/specs/contact_save_spec.js
+++ frontend/tests/e2e/specs/contact_save_spec.js
@@ -1,12 +1,12 @@
 describe('Cadastro de Contatos', () => {
 
-    const user = { email: 'patricia@gmail.com', password: 'pwd123'}
+    const user = { email: 'patricia@gmail.com', password: 'pwd123' }
 
     before(()=> {
         cy.request({
             method: 'POST',
             url: 'http://localhost:3000/user',
-            headers: { 'Content-Type': 'application/json'},
+            headers: { 'Content-Type': 'application/json' },
             body: user,
             failOnStatusCode: false
         }).then((response) => {
@@ -14,14 +14,14 @@ describe('Cadastro de Contatos', () => {
         })
 
         cy.doLogin(user.email, user.password)
-        cy.get('.dashboard', { timeout: 5000}).should('be.visible')
+        cy.get('.dashboard', {timeout: 5000}).should('be.visible')
     })
 
     describe('Novo Contato', () => {
         let contact = {
-            name: 'Everton Rafael de Souza',
-            number: '199999999999',
-            description: 'Solicitar orçamento para Consultoria em QA e DevOps.'
+            name: 'Everton Souza',
+            number: '199888877776',
+            description: 'Solicitar orçamento para Consultoria em QA.'
         }
 
         describe('Quando submeto o cadastro completo', () => {
@@ -38,56 +38,62 @@ describe('Cadastro de Contatos', () => {
         describe('Quando submeto o cadastro sem o nome', () => {
 
             let contact = {
-                number: '199999999999',
-                description: 'Solicitar orçamento para Consultoria em QA e DevOps.'
+                number: '199888877776',
+                description: 'Solicitar orçamento para Consultoria em QA.'
             }
 
-            const expectNotice = 'Nome é obrigatório'
+            const expectNotice = 'Nome é Obrigatório'
 
             before(() => {
                 cy.dash()
                 cy.createContact(contact)
             })
 
-            it('Deve exibir a notificação: ${expectNotice}', () => {
+            it(`Deve exibir a notificação ${expectNotice.replace(/[^a-zA-Z ]/g, '')}`, () => {
                 cy.alertName().contains(expectNotice)
             })
         })
 
         describe('Quando submeto o cadastro sem o whatsapp', () => {
+
             let contact = {
-                name: 'Everton Rafael de Souza',
-                description: 'Solicitar orçamento para Consultoria em QA e DevOps.'
+                name: 'Everton Souza',
+                description: 'Solicitar orçamento para Consultoria em QA.'
             }
 
-            const expectNotice = 'WhatsApp é obrigatório'
-
+            const expectNotice = 'Whatsapp é Obrigatório'
+    
             before(() => {
                 cy.dash()
                 cy.createContact(contact)
             })
 
-            it('Deve exibir a notificação: ${expectNotice}', () => {
+            it(`Deve exibir a notificação ${expectNotice.replace(/[^a-zA-Z ]/g, '')}`, () => {
                 cy.alertNumber().contains(expectNotice)
             })
         })
 
         describe('Quando submeto o cadastro sem o assunto', () => {
+
             let contact = {
-                name: 'Everton Rafael de Souza',
-                number: '199999999999'
+                name: 'Everton Souza',
+                number: '199888877776'
             }
 
-            const expectNotice = 'Assunto é obrigatório.'
-            
+            const expectNotice = 'Assunto é Obrigatório'
+
             before(() => {
                 cy.dash()
                 cy.createContact(contact)
             })
 
-            it('Deve exibir a notificação: ${expectNotice}', () => {
+            it(`Deve exibir a notificação ${expectNotice.replace(/[^a-zA-Z ]/g, '')}`, () => {
                 cy.alertDesc().contains(expectNotice)
             })
         })
+
+
+
     })
+
 })
--- frontend/tests/e2e/support/index.js
+++ frontend/tests/e2e/support/index.js
@@ -27,3 +27,11 @@ Cypress.Screenshot.defaults({
 afterEach(() => {
     cy.screenshot()
 })
+
+const addContext = require('mochawesome/addContext')
+
+Cypress.on('test:after:run', (test) => {    
+    const shotFileName = `${test.title} -- after each hook.png`    
+    addContext({test}, `assets/screenshots/${Cypress.spec.name}/${shotFileName}`)
+})
+
